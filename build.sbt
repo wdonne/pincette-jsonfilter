f69723c1c5528d090248ce6deeb0ede6ff0a6080
Werner Donné <werner.donne@pincette.biz>
2018-12-06 18:11:21 +0100

    Ongoing

--- build.sbt
+++ build.sbt
@@ -1,12 +1,12 @@
 name := """pincette-jsonfilter"""
 organization := "net.pincette"
-version := "0.0.1"
+version := "0.0.2"
 
 scalaVersion := "2.12.4"
 
 libraryDependencies ++= Seq(
-  "javax.json" % "javax.json-api" % "1.1.2",
-  "net.pincette" % "pincette-common" % "1.2.6"
+  "javax.json" % "javax.json-api" % "1.1.4",
+  "net.pincette" % "pincette-common" % "1.3.4"
 )
 
 publishTo := Some(Resolver.file("file",  new File(Path.userHome.absolutePath + "/.m2/repository")))
---
+++ src/main/java/net/pincette/jf/JsonBuilderGenerator.java
@@ -0,0 +1,234 @@
+package net.pincette.jf;
+
+import static javax.json.Json.createArrayBuilder;
+import static javax.json.Json.createObjectBuilder;
+import static javax.json.Json.createValue;
+import static net.pincette.util.Pair.pair;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Optional;
+import java.util.function.Consumer;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonException;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonValue;
+import javax.json.stream.JsonGenerator;
+import net.pincette.util.Pair;
+
+/**
+ * Accumulates a JSON stream in a given JSON builder.
+ *
+ * @author Werner Donn\u00e9
+ */
+public class JsonBuilderGenerator implements JsonGenerator {
+  private Deque<Pair<String, Object>> builders = new ArrayDeque<>();
+  private String lastName;
+
+  public JsonBuilderGenerator(final JsonObjectBuilder builder) {
+    builders.push(pair(null, builder));
+  }
+
+  public JsonBuilderGenerator(final JsonArrayBuilder builder) {
+    builders.push(pair(null, builder));
+  }
+
+  private static void add(final Object parent, final Pair<String, Object> builder) {
+    if (builder.first != null) {
+      ((JsonObjectBuilder) parent).add(builder.first, build(builder.second));
+    } else {
+      ((JsonArrayBuilder) parent).add(build(builder.second));
+    }
+  }
+
+  private static JsonValue build(final Object builder) {
+    return builder instanceof JsonObjectBuilder
+        ? ((JsonObjectBuilder) builder).build()
+        : ((JsonArrayBuilder) builder).build();
+  }
+
+  private Optional<JsonObjectBuilder> asObjectBuilder() {
+    return Optional.ofNullable(builders.peek()).map(p -> (JsonObjectBuilder) p.second);
+  }
+
+  private Optional<JsonArrayBuilder> asArrayBuilder() {
+    return Optional.ofNullable(builders.peek()).map(p -> (JsonArrayBuilder) p.second);
+  }
+
+  private void checkNoLastName() {
+    if (lastName != null) {
+      throw new JsonException("writeKey was called without following value");
+    }
+  }
+
+  public void close() {
+    // Nothing to close.
+  }
+
+  public void flush() {
+    // Nothing to flush.
+  }
+
+  public JsonGenerator write(final boolean value) {
+    write(value ? JsonValue.TRUE : JsonValue.FALSE);
+
+    return this;
+  }
+
+  public JsonGenerator write(final double value) {
+    write(createValue(value));
+
+    return this;
+  }
+
+  public JsonGenerator write(final int value) {
+    write(createValue(value));
+
+    return this;
+  }
+
+  public JsonGenerator write(final long value) {
+    write(createValue(value));
+
+    return this;
+  }
+
+  public JsonGenerator write(final String value) {
+    write(createValue(value));
+
+    return this;
+  }
+
+  public JsonGenerator write(final BigDecimal value) {
+    write(createValue(value));
+
+    return this;
+  }
+
+  public JsonGenerator write(final BigInteger value) {
+    write(createValue(value));
+
+    return this;
+  }
+
+  public JsonGenerator write(final JsonValue value) {
+    writeAnonymous(
+        name -> asObjectBuilder().ifPresent(b -> b.add(name, value)),
+        () -> asArrayBuilder().ifPresent(b -> b.add(value)));
+
+    return this;
+  }
+
+  public JsonGenerator write(final String name, final boolean value) {
+    write(name, value ? JsonValue.TRUE : JsonValue.FALSE);
+
+    return this;
+  }
+
+  public JsonGenerator write(final String name, final double value) {
+    write(name, createValue(value));
+
+    return this;
+  }
+
+  public JsonGenerator write(final String name, final int value) {
+    write(name, createValue(value));
+
+    return this;
+  }
+
+  public JsonGenerator write(final String name, final long value) {
+    write(name, createValue(value));
+
+    return this;
+  }
+
+  public JsonGenerator write(final String name, final String value) {
+    write(name, createValue(value));
+
+    return this;
+  }
+
+  public JsonGenerator write(final String name, final BigDecimal value) {
+    write(name, createValue(value));
+
+    return this;
+  }
+
+  public JsonGenerator write(final String name, final BigInteger value) {
+    write(name, createValue(value));
+
+    return this;
+  }
+
+  public JsonGenerator write(final String name, final JsonValue value) {
+    checkNoLastName();
+    asObjectBuilder().ifPresent(b -> b.add(name, value));
+
+    return this;
+  }
+
+  private <T> void writeAnonymous(
+      final Consumer<String> objectBuilder, final Runnable arrayBuilder) {
+    if (lastName != null) {
+      objectBuilder.accept(lastName);
+      lastName = null;
+    } else {
+      arrayBuilder.run();
+    }
+  }
+
+  public JsonGenerator writeEnd() {
+    final Pair<String, Object> builder = builders.pop();
+
+    Optional.ofNullable(builders.peek()).map(p -> p.second).ifPresent(b -> add(b, builder));
+
+    return this;
+  }
+
+  public JsonGenerator writeKey(final String name) {
+    lastName = name;
+
+    return this;
+  }
+
+  public JsonGenerator writeNull() {
+    write(JsonValue.NULL);
+
+    return this;
+  }
+
+  public JsonGenerator writeNull(final String name) {
+    write(name, JsonValue.NULL);
+
+    return this;
+  }
+
+  public JsonGenerator writeStartArray() {
+    builders.push(pair(lastName, createArrayBuilder()));
+
+    return this;
+  }
+
+  public JsonGenerator writeStartArray(final String name) {
+    checkNoLastName();
+    builders.push(pair(name, createArrayBuilder()));
+
+    return this;
+  }
+
+  public JsonGenerator writeStartObject() {
+    builders.push(pair(lastName, createObjectBuilder()));
+
+    return this;
+  }
+
+  public JsonGenerator writeStartObject(final String name) {
+    checkNoLastName();
+    builders.push(pair(name, createObjectBuilder()));
+
+    return this;
+  }
+}
